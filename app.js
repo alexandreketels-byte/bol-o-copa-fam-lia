8ac64c591688463f29312c73e649fb2e2da96c55
alexandreketels-byte <alexandreketels@gmail.com>
2026-06-25 21:43:45 -0300

    Update app.js

--- app.js
+++ app.js
@@ -169,11 +169,8 @@ function renderizar() {
         <div class="bola"></div>
         <div>
           <h1>Bolão da Copa</h1>
-          <div class="sub premios-sub">
-  <span>🥇 R$ 100,00</span>
-  <span>🥈 R$ 20,00</span>
-  <span>🥉 R$ 10,00</span>
-</div>
+          <div class="sub">⚽⚽   🥇R$100,00  🥈R$20,00 🥉R$10,00</div>
+        </div>
       </div>
       <div class="usuario-chip">
         <img src="${eu?.foto || fotoPadrao()}" onerror="this.src='${fotoPadrao()}'">
